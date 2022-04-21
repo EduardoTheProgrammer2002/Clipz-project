d7c091c5866dae4f7122c4a693d571d5f41a5830
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-04-21 16:51:11 -0700

    clips project( lesson 69 - lesson 84 )

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+  private modals: IModal[] = [];
+
+  constructor() { }
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    });
+  }
+
+  isModalOpen():boolean {
+    return true
+  }
+
+  toggleModal():void {
+    // this.visible = !this.visible;
+  }
+}
