0bac5c0116a92e86bb5d9b27bd9840f3e5c5717d
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-04-27 15:39:25 -0700

    Lesson 116 - Designing an alert component to give a feedback when the form has been submitted.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -32,6 +32,10 @@ export class RegisterComponent {
     Validators.maxLength(13)
   ])
 
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being created.'
+  alertColor = 'blue'
+
   registerForm = new FormGroup({
      name: this.name,
      email: this.email,
@@ -42,6 +46,8 @@ export class RegisterComponent {
   })
 
   register() {
-    console.log("Form submitted.")
+    this.showAlert = true
+    this.alertMsg = 'Please wait! Your account is being created.'
+    this.alertColor = 'blue'
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  safelist: ['bg-red-400', 'bg-green-400', 'bg-blue-400'],
   theme: {
     extend: {},
   },
