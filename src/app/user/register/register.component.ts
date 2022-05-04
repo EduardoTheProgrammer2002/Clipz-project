ab7ef534c95161399f2e8af889089c31242600d7
EduardoTheProgrammer2002 <edwardo16blak@gmail.com>
2022-05-04 16:30:30 -0400

    Lesson 53 - User Registration in firebase.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(private auth: AngularFireAuth) {}
+
   name = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -45,9 +48,16 @@ export class RegisterComponent {
      phoneNumber: this.phoneNumber
   })
 
-  register() {
+  async register() {
     this.showAlert = true
     this.alertMsg = 'Please wait! Your account is being created.'
     this.alertColor = 'blue'
+
+    const { email, password } = this.registerForm.value
+    const userCred = this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    )
+
   }
 }
