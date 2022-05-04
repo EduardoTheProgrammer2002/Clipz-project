28ac685b0d9d6cc91f1a3c61b30ad775ba8b4ed6
EduardoTheProgrammer2002 <edwardo16blak@gmail.com>
2022-05-04 15:44:23 -0400

    Lesson 151 to 152 - Installing Angularfire and configuring the environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD_zm3_wid59T3jebPXltHHyNhh4v5Psz4",
+    authDomain: "clipz-e8b81.firebaseapp.com",
+    projectId: "clipz-e8b81",
+    storageBucket: "clipz-e8b81.appspot.com",
+    appId: "1:755612767831:web:1e242f2c15215e6e8db512"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD_zm3_wid59T3jebPXltHHyNhh4v5Psz4",
+    authDomain: "clipz-e8b81.firebaseapp.com",
+    projectId: "clipz-e8b81",
+    storageBucket: "clipz-e8b81.appspot.com",
+    appId: "1:755612767831:web:1e242f2c15215e6e8db512"
+  }
 };
 
 /*
