00251da31541f4bb4f5d409a568a37609b449701
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-05-09 11:56:36 -0700

    Lesson 176 - Handling errors.

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,17 +1,22 @@
 import { ValidationErrors, AbstractControl, ValidatorFn } from "@angular/forms";
 export class RegisterValidators {
+    
+
     static match(nameControl: string, matchingNameControl: string): ValidatorFn {
         return (group: AbstractControl): ValidationErrors | null => {
             const control = group.get(nameControl);
             const matchingControl = group.get(matchingNameControl)
             
             if (!control || !matchingControl) {
+                console.log('Form control can not be found in the form group.')
                 return { controlNotFound: false }
             }
     
             const error = control.value === matchingControl.value ? 
                 null :
                 { noMatch: true }
+
+            matchingControl.setErrors(error);
             
             return error;
         }
